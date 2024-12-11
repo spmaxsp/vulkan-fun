91a2a786916a375ad7953a26061275b1f378fa1d
spmaxsp <mexx@spahn-rodgau.de>
2024-12-11 23:38:46 +0100

    adding function to check vulkan layer support

--- src/vulkan_base/vulkan_device.cpp
+++ src/vulkan_base/vulkan_device.cpp
@@ -1,8 +1,51 @@
 #include "vulkan_base.h"
 
 
+bool checkLayerSupport(std::vector<const char*> requiredLayers) {
+
+    // Get list of available layers
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    // Log the available layers
+    spdlog::info("Available layers:");
+    for (const auto& layer : availableLayers) {
+        spdlog::info(" - \"{0}\"", layer.layerName);
+        spdlog::info("        {0}", layer.description);
+    } 
+
+    // check if all requested layers are available
+    for (const char* requiredLayer : requiredLayers) {
+        bool layerFound = false;
+        for (const auto& layer : availableLayers) {
+            if (strcmp(requiredLayer, layer.layerName) == 0) {
+                spdlog::info("Layer \"{0}\" found", requiredLayer);
+                layerFound = true;
+                break;
+            }
+        }
+        if (!layerFound) {
+            spdlog::error("Layer \"{0}\" not found", requiredLayer);
+            return false;
+        }
+    }
+    return true;
+
+}
+
+
 bool initVulkanInstance(VulkanContext* context) {
 
+    // check layer support
+    const std::vector<const char*> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"
+    };
+    if (!checkLayerSupport(validationLayers)) {
+        spdlog::info("Validation layers requested, but not available!");
+        return false;
+    }
 
     // specify application information
     VkApplicationInfo applicationInfo = {VK_STRUCTURE_TYPE_APPLICATION_INFO};
@@ -14,13 +57,13 @@ bool initVulkanInstance(VulkanContext* context) {
     VkInstanceCreateInfo createInfo = {VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO};
     createInfo.pApplicationInfo = &applicationInfo;
     createInfo.enabledLayerCount = 0;
-    createInfo.ppEnabledLayerNames = 0;
+    createInfo.ppEnabledLayerNames = nullptr;
     createInfo.enabledExtensionCount = 0;
-    createInfo.ppEnabledExtensionNames = 0;
+    createInfo.ppEnabledExtensionNames = nullptr;
 
     // create a new Vulkan instance using the supplied information
     spdlog::info("creating Vulkan instance");
-    if (vkCreateInstance(&createInfo, 0, &context->instance) != VK_SUCCESS) {
+    if (vkCreateInstance(&createInfo, nullptr, &context->instance) != VK_SUCCESS) {
         spdlog::error("Error creating Vulkan instance");
         return false;
     }
